f97f7d4fde02967c43f62c2ffdfdfbe54565cbcf
lukas-kucera <kucera-lukas@users.noreply.github.com>
2022-01-24 14:01:14 +0100

    update linting

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: ["src"],
 };
 
 module.exports = nextConfig;
