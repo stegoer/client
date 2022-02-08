5874f15c4c03bddce67c60bdf5cda04467c165d9
lukas-kucera <kucera-lukas@users.noreply.github.com>
2022-02-08 11:15:56 +0100

    separate component for head

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,33 +1,12 @@
 import React from "react";
 import type { NextPage } from "next";
 import Link from "next/link";
-import Head from "next/head";
+import Head from "../components/Head";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>stegoer</title>
-        <link rel="manifest" href="site.webmanifest.json" />
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
-      </Head>
+      <Head />
       <div>
         <h1>stegoer</h1>
         <ol>
