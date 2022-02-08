9dbfeeeb1c23415f1c9bb6fe7c2d8739f4bfad98
lukas-kucera <kucera-lukas@users.noreply.github.com>
2022-02-08 12:00:03 +0100

    add color scheme via mantine

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,48 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import GraphqlProvider from "../graphql/provider";
+import Head from "../components/Head";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import {
+  useColorScheme,
+  useHotkeys,
+  useLocalStorageValue,
+} from "@mantine/hooks";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function Stegoer({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useLocalStorageValue<ColorScheme>({
+    key: `mantine-color-scheme`,
+    defaultValue: useColorScheme(),
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === `dark` ? `light` : `dark`));
+
+  useHotkeys([[`mod+J`, () => toggleColorScheme()]]);
+
   return (
-    <GraphqlProvider>
-      <Component {...pageProps} />
-    </GraphqlProvider>
+    <>
+      <Head />
+      <GraphqlProvider>
+        <ColorSchemeProvider
+          colorScheme={colorScheme}
+          toggleColorScheme={toggleColorScheme}
+        >
+          <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{ colorScheme }}
+          >
+            <Component {...pageProps} />
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </GraphqlProvider>
+    </>
   );
 }
 
-export default MyApp;
+export default Stegoer;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,37 +1,33 @@
 import React from "react";
 import type { NextPage } from "next";
 import Link from "next/link";
-import Head from "../components/Head";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Head />
-      <div>
-        <h1>stegoer</h1>
-        <ol>
-          <li>
-            <Link href="/login">
-              <a>Login</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/encode">
-              <a>Encode</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/decode">
-              <a>Decode</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/images">
-              <a>Images</a>
-            </Link>
-          </li>
-        </ol>
-      </div>
+      <h1>stegoer</h1>
+      <ol>
+        <li>
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/encode">
+            <a>Encode</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/decode">
+            <a>Decode</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/images">
+            <a>Images</a>
+          </Link>
+        </li>
+      </ol>
     </div>
   );
 };
