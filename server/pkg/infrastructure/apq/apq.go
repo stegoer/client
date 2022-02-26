e45e4682c94741bbd3b39602ed3593aa6924e665
lukas-kucera <kucera-lukas@users.noreply.github.com>
2022-02-26 18:17:12 +0100

    add comments

--- server/pkg/infrastructure/apq/apq.go
+++ server/pkg/infrastructure/apq/apq.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
+// Cache represents a redis.Client for Automatic Persisted Queries.
 type Cache struct {
 	client redis.Client
 	ttl    time.Duration
@@ -17,14 +18,17 @@ const (
 	TTL       = time.Hour * 24
 )
 
+// NewCache returns a new Cache instance for Automatic Persisted Queries.
 func NewCache(client redis.Client) *Cache {
 	return &Cache{client: client, ttl: TTL}
 }
 
+// Add adds a new k/v pair to the Cache instance.
 func (c *Cache) Add(ctx context.Context, key string, value interface{}) {
 	c.client.Set(ctx, getKey(key), value, c.ttl)
 }
 
+// Get retrieves a value from the Cache instance.
 func (c *Cache) Get(ctx context.Context, key string) (interface{}, bool) {
 	res, err := c.client.Get(ctx, getKey(key)).Result()
 	if err != nil {
