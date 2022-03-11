562558a4e2eda6cf1bc44b61adbb394bdf8745d2
lukas-kucera <kucera-lukas@users.noreply.github.com>
2022-03-11 09:52:45 +0100

    generate client docs

---
+++ client/docs/assets/search.js
@@ -0,0 +1,3 @@
+window.searchData = JSON.parse(
+  '{"kinds":{"4":"Namespace","32":"Variable"},"rows":[{"id":0,"kind":32,"name":"default","url":"modules.html#default","classes":"tsd-kind-variable"},{"id":1,"kind":4,"name":"<internal>","url":"modules/_internal_.html","classes":"tsd-kind-namespace"}],"index":{"version":"2.3.9","fields":["name","parent"],"fieldVectors":[["name/0",[0,6.931]],["parent/0",[]],["name/1",[1,6.931]],["parent/1",[]]],"invertedIndex":[["default",{"_index":0,"name":{"0":{}},"parent":{}}],["internal",{"_index":1,"name":{"1":{}},"parent":{}}]],"pipeline":[]}}',
+);
