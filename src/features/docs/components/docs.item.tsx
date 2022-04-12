ecbdd0fb753ec19084078db2425beaa549bf3e47
Lukas Kucera <kucera-lukas@users.noreply.github.com>
2022-04-12 20:56:42 +0200

    update documentation urls

--- src/features/docs/components/docs.item.tsx
+++ src/features/docs/components/docs.item.tsx
@@ -1,21 +1,35 @@
 import { List } from "@mantine/core";
-import { DOCUMENTATION_URLS, SOURCE_URLS } from "@features/docs/docs.constants";
+import {
+  DEVELOPMENT_DOCUMENTATION_URLS,
+  REFERENCE_URLS,
+  SOURCE_URLS,
+} from "@features/docs/docs.constants";
 import type { DocType } from "@features/docs/docs.types";
 import TextLink from "@components/navigation/text.link";
-import { GitHubLogoIcon, ReaderIcon } from "@modulz/radix-icons";
+import { FileIcon, GitHubLogoIcon, ReaderIcon } from "@modulz/radix-icons";
 
 export type DocsItemProps = {
   docType: DocType;
 }
 
 const DocsItem = ({ docType }: DocsItemProps): JSX.Element => {
+  const referenceUrl = REFERENCE_URLS[docType]
+
   return (
     <List center>
       <List.Item icon={<ReaderIcon/>} >
-        <TextLink href={DOCUMENTATION_URLS[docType]}>
-          documentation
+        <TextLink href={DEVELOPMENT_DOCUMENTATION_URLS[docType]}>
+          development
         </TextLink>
       </List.Item>
+      {referenceUrl && (
+        <List.Item icon={<FileIcon />}>
+          <TextLink href={referenceUrl}>
+            reference
+          </TextLink>
+        </List.Item>
+      )
+      }
       <List.Item icon={<GitHubLogoIcon/>}>
         <TextLink href={SOURCE_URLS[docType]}>
           source
